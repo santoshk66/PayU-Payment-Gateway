575479f8b74ed26f7052ff131e2417cc7ec65c50
santoshk66 <santosh.k66@gmail.com>
2025-07-25 18:15:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const crypto = require('crypto');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const { PAYU_MERCHANT_KEY, PAYU_MERCHANT_SALT } = process.env;
 
